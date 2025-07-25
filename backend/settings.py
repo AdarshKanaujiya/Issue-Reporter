8d5955dc555d4f6f68c2453a1ab23cfe3284c805
AdarshKanaujiya <adarkanjiya@gmail.com>
2025-07-25 14:13:47 +0530

    Ready for Render deployment

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['issue-reporter-uazm.onrender.com']
 
 
 # Application definition
